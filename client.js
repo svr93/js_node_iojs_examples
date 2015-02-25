dab07bf311205e92309115c10e4d7158d15a60ab
Владимир <svr93@i.ua>
2015-02-25 20:51:02 +0400

    set_base_arch

---
+++ client.js
@@ -0,0 +1,34 @@
+/* ----- base modules ----- */
+
+var http = require('http');
+var https = require('https');
+
+/* ----- additional modules ----- */
+
+var htmlparser = require('htmlparser');
+
+var baseHandler = new htmlparser.DefaultHandler(function (error, dom) {
+
+});
+
+var parser = new htmlparser.Parser(baseHandler);
+
+/* ----- command line args reading ----- */
+
+/* it takes to set 'https' to use https-connection
+   or set 'http'/nothing to use http-connection;
+   both parameters are prohibited */
+
+if (process.argv.indexOf('https') != -1 && process.argv.indexOf('http') == -1) {
+  setHttpsParams();
+} else if (process.argv.indexOf('https') == -1) {
+  setHttpParams();
+} else console.log('error');
+
+function setHttpParams() {
+  console.log('http_params');
+}
+
+function setHttpsParams() {
+  console.log('https_params');
+}
